128789ac967d129702b43ff53f17e22bc3974e9e
tanghuibo <1215893264@qq.com>
2022-09-09 18:37:50 +0800

    Initial commit

---
+++ src/main/java/com/iqiyi/intl/memorytest/MemoryTestApplication.java
@@ -0,0 +1,41 @@
+package com.iqiyi.intl.memorytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class MemoryTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryTestApplication.class, args);
+	}
+
+	List<byte[]> list = new ArrayList<>();
+
+	@GetMapping("check")
+	public String check() {
+		return "ok";
+	}
+
+	@GetMapping("add")
+	public String add(@RequestParam("size") Integer size) {
+		list.add(new byte[size]);
+		return "success";
+	}
+
+
+
+	@GetMapping("clear")
+	public String clear() {
+		list.clear();
+		return "success";
+	}
+
+}
---
+++ src/test/java/com/iqiyi/intl/memorytest/MemoryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqiyi.intl.memorytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
